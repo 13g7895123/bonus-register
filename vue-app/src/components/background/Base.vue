28bb0ab2fdcf6c052f349d47101c74865fa77581
Jarvis <13g7895123@gmail.com>
2023-08-20 22:09:21 +0800

    [edit] test render

--- vue-app/src/components/background/Base.vue
+++ vue-app/src/components/background/Base.vue
@@ -6,7 +6,7 @@
             <!-- title -->
             <h2 class="text-white mt-12">帳號註冊</h2>
             <!-- <PhoneValidation /> -->
-            <!-- <slot name="form"></slot> -->
+            <slot name="form"></slot>
         </div>
     </div>
 </template>
