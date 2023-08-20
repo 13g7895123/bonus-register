4b1ad36e9c0f6ee062e606dd5b9503c2e19fac9b
Jarvis <13g7895123@gmail.com>
2023-08-20 20:22:27 +0800

    [add] show now route

--- vue-app/src/views/Base.vue
+++ vue-app/src/views/Base.vue
@@ -6,34 +6,20 @@
             <!-- title -->
             <h2 class="text-white mt-12">帳號註冊</h2>
             <PhoneValidation />
-            <!-- <form id="form_area">
-                <div class="inp_group mt-12">
-                    <input v-model = 'phone' required>
-                    <span class="">手機號碼</span>
-                    <i></i>
-                </div>
-                <div class="inp_group mt-2">
-                    <input v-model = 'code' required>
-                    <span>認證碼</span>
-                    <i style="width: 60%;"></i>
-                    <div id='btn_send_code' class="btn" @click="sendCode">發送認證碼</div>
-                </div>
-                <div class="bg-white rounded flex justify-center items-center btn py-1 mb-2 mt-6">送出</div>
-                <hr class="mt-5">
-                <div 
-                    class="bg-white rounded flex justify-center items-center btn py-1 mt-5"
-                    @click = "router.push('/checkAccount')"
-                    >查詢帳號</div>
-                <div
-                    class="bg-white rounded flex justify-center items-center btn py-1 mt-3"
-                    @click = "router.push('/forgetPassword')"
-                    >找回密碼</div>
-            </form> -->
         </div>
     </div>
 </template>
 <script setup>
+import { useRoute } from 'vue-router'
+import { computed, onMounted } from 'vue'
 import PhoneValidation from './PhoneValidation.vue';
+
+const route = useRoute()
+const path  = computed(() => route.path)
+
+onMounted(() => {
+    console.log(path);
+})
 
 </script>
 <style>
