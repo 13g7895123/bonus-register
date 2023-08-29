8765f4389ff11b12c61619d2a2a848ada3747603
Jarvis <13g7895123@gmail.com>
2023-08-29 22:54:35 +0800

    [edit] add transform server name

--- vue-app/src/components/background/BasePhoneValidation.vue
+++ vue-app/src/components/background/BasePhoneValidation.vue
@@ -4,7 +4,7 @@
         <div id="login-box" class="rounded-lg">
             <div id="mask"></div>
             <h2 class="text-white mt-12">註冊帳號</h2>
-            <h3 id='server_name' class="text-white">【{{ server }}】</h3>
+            <h3 id='server_name' class="text-white">【{{ server_name }}】</h3>
             <PhoneValidation />
         </div>
     </div>
@@ -13,13 +13,28 @@
 import PhoneValidation from '../form/PhoneValidation.vue';
 import { onMounted, ref } from 'vue'
 import { useRouter } from "vue-router";
+import axios from 'axios'
 
 const router = useRouter()
 const server = ref()
+const server_name = ref()
 
-onMounted(() => {
+onMounted(async() => {
     server.value = router.currentRoute._value.params.server
-    console.log(server.value);
+    
+    const formData = ref({
+        server: server.value,
+    })
+    apiParam.value = '?action=server_name'
+    apiUrl.value = apiUrlPrefix + "common.php" + apiParam.value
+
+    const { data: { success, data } } = await axios.post(
+        apiUrl.value, formData.value
+    )
+
+    if (success){
+        server_name.value = data;
+    }
 })
 
 </script>
