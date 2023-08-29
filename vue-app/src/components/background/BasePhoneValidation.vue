d059436939cc17dd53db622308be1e0228441ef6
Jarvis <13g7895123@gmail.com>
2023-08-29 22:27:01 +0800

    [edit] fix route error

--- vue-app/src/components/background/BasePhoneValidation.vue
+++ vue-app/src/components/background/BasePhoneValidation.vue
@@ -4,7 +4,7 @@
         <div id="login-box" class="rounded-lg">
             <div id="mask"></div>
             <h2 class="text-white mt-12">註冊帳號</h2>
-            <h3 class="text-white">【{{ server.value }}】</h3>
+            <h3 class="text-white">【{{ server }}】</h3>
             <PhoneValidation />
         </div>
     </div>
