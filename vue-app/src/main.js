29675bb39f4bde46e98f25283acc09ef4de718b4
ict005 <13g7895123@gmail.com>
2023-08-31 12:34:37 +0800

    [edit] import datepicker

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -4,10 +4,13 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import route from "./router"
 import 'vuetify/dist/vuetify.min.css';
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 loadFonts()
 
 const app = createApp(App)
 app.use(vuetify)
 app.use(route)
+app.component('VueDatePicker', VueDatePicker)
 app.mount('#app')
