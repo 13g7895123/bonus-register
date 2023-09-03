bdf38151709ff3a5c7d84a155ca740d009e57573
Jarvis <13g7895123@gmail.com>
2023-09-03 15:27:54 +0800

    [edit] test fix render

--- vue-app/src/components/background/BaseRegister.vue
+++ vue-app/src/components/background/BaseRegister.vue
@@ -58,7 +58,6 @@ const get_server = async() => {
     display: flex;
     flex-direction: column;
     align-items: center;
-    /* backdrop-filter: blur(10px); */
     z-index: 999;
     background: transparent;
 }
