98e1aba3efa97e1dd4229ca3a8880d0c1f2c3eb0
Jarvis <13g7895123@gmail.com>
2023-09-19 00:19:49 +0800

    [edit] test

--- vue-app/src/router/routes.js
+++ vue-app/src/router/routes.js
@@ -2,7 +2,7 @@ const routes = [
     {
         path: "/",
         name: 'Home',
-        component: () => import('../views/Base.vue'),
+        component: () => import('../views/Test.vue'),
     },
     {
         path: "/phonevalidation/:server",
